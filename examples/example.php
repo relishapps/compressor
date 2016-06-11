a3b1b8e21a0293afaa5bbfbf3fa02500d9efda73
Pawel Decowski <pawel.decowski@gmail.com>
2016-06-11 21:52:47 +0100

    Fix example to work with web root one level up from compressor dir

--- examples/example.php
+++ examples/example.php
@@ -1,12 +1,20 @@
 <?php
-require_once 'compressor.php';
+require_once '../vendor/autoload.php';
+
+use RelishMedia\Compressor;
+
+// Change this to your web server root
+define('COMPRESSOR_ROOT', dirname(dirname(__DIR__)));
+
+// Path to assets dir, relative to `COMPRESSOR_ROOT`
+define('COMPRESSOR_URL', '/compressor/examples/');
 ?>
 
 <head>
     <?php Compressor::start() ?>
-    <link rel="stylesheet" href="css/file1.css">
-    <link rel="stylesheet" href="css/file2.css">
-    <script src="js/file1.js"></script>
-    <script src="js/file2.js"></script>
+    <link rel="stylesheet" href="/compressor/examples/css/file1.css">
+    <link rel="stylesheet" href="/compressor/examples/css/file2.css">
+    <script src="/compressor/examples/js/file1.js"></script>
+    <script src="/compressor/examples/js/file2.js"></script>
     <?php Compressor::stop() ?>
 </head>
